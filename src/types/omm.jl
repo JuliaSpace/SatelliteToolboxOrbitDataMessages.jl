7f13afeec2e7bac8483ec57222e2a996a7c89500
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2026-07-09 18:40:40 -0300

    :bug: Export OmmCovarianceMatrix

--- src/types/omm.jl
+++ src/types/omm.jl
@@ -9,7 +9,7 @@
 #
 ############################################################################################
 
-export OrbitMeanElementsMessage
+export OrbitMeanElementsMessage, OmmCovarianceMatrix
 
 # == Types =================================================================================
 
